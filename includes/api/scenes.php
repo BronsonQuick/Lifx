3a45b103bc089341b063bef5db5ed467c2b835cf
Bronson Quick <bronson@bronsonquick.com.au>
2022-12-30 11:43:10 +1000

    Add list_scenes command

--- includes/api/scenes.php
+++ includes/api/scenes.php
@@ -1,2 +1,25 @@
 <?php
 namespace Lifx\Scenes;
+/**
+ * A function to list all the scenes associated with your LIFX Cloud account.
+ *
+ * @return array[]|mixed|\WP_Error
+ */
+function list_scenes() {
+	$headers = \Lifx\Auth\get_headers();
+
+	$endpoint = LIFX_ENDPOINT . "/scenes";
+
+	if ( is_wp_error( $headers ) ) {
+		return $headers;
+	}
+
+	$scenes = wp_safe_remote_get(
+		$endpoint,
+		$headers
+	);
+
+	$response = json_decode( wp_remote_retrieve_body( $scenes ), true );
+
+	return $response;
+}
